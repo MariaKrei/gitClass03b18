58f1731071ce259f2bf283185cde6847b1dd121f
Maria Krei <kreilichmaria@gmail.com>
2023-12-05 20:13:26 -0600

    3rd commit

--- src/main/java/NEW/C2.java
+++ src/main/java/NEW/C2.java
@@ -3,6 +3,7 @@ package NEW;
 public class C2 {
     public static void main(String[] args) {
         System.out.println("Git is easy");
-        String name="Maria"
+        String name="Maria";
+        System.out.println("New code to commit");
     }
 }
